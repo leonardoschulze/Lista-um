97bc02ec86c34d2c802073a713336d2c6cb75059
leonardoschulze <leonardo_schulze@estudante.sesisenai.org.br>
2025-07-07 11:15:39 -0300

    ADD - feito o exercicio quatro

---
+++ exercicio04.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informe Número</title>
+</head>
+<body>
+
+    <form method="POST" action="">
+        <label for="numero">Informe um Número :</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar">Verificar</button>
+    </form>
+
+    <?php
+        if($_SERVER['REQUEST_METHOD']== 'POST'){
+            
+            if(isset($_POST['verificar']));
+            $numero = $_POST['numero'];
+            $resultado = 1;
+            
+            for($i = 1; $i <= $numero; $i++){
+                $resultado = $resultado * $i;
+            };
+            echo "$resultado <br>";
+        };
+    ?>
+    
+</body>
+</html>
